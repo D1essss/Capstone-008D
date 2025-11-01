c4f725490af757f04ab7f7d42e6ab5cedb082210
naxoski <i.huerta@duocuc.cl>
2025-10-31 21:16:55 -0300

    Fixes

--- Proyecto/main.js
+++ Proyecto/main.js
@@ -3,7 +3,7 @@
 // Módulos para controlar la aplicación y crear ventanas
 const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('node:path');
-require('update-electron-app')();
+__non_webpack_require__('update-electron-app')();
 
 // Función que crea la ventana principal del navegador.
 const createWindow = () => {
