54bf6251fb9f247cd716d3679bff5d4300691934
naxoski <i.huerta@duocuc.cl>
2025-10-31 21:59:40 -0300

    Me volvere loco

--- Proyecto/main.js
+++ Proyecto/main.js
@@ -1,4 +1,5 @@
 // main.js
+if (require('electron-squirrel-startup')) app.quit();
 
 // Módulos para controlar la aplicación y crear ventanas
 const { app, BrowserWindow, ipcMain } = require('electron');
